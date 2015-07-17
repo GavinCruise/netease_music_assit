f68cce6ca1dea966aea1a1350e00723a6e4ce2de
zhour <zhourongfaith@gmail.com>
2015-07-17 11:49:44 +0800

    初步架构

---
+++ create_list.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+from urllib import urlencode
+import json
+from musician import musician
+
+cookies = {"os": "osx"}
+header = {
+            "Host": "music.163.com", "Content-Type": "application/x-www-form-urlencoded",
+            "Origin": "orpheus://orpheus",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.78.2 (KHTML, like Gecko)"
+            }
+
+def get_user_id_by_name(name):
+    url = "http://music.163.com/api/search/pc"
+
+    params = {"type": "1002", "s": name, "offset": "0", "total": "true", "limit": "1"}
+    print(urlencode(params))
+    resp = requests.post(url, urlencode(params), cookies=cookies, headers=header)
+    musician_r = resp.json()
+    m = musician()
+    print(json.dumps(musician_r, ensure_ascii=False))
+    profile = musician_r["result"]["userprofiles"][0]
+    m.userId = profile["userId"]
+    m.avatarUrl = profile["avatarUrl"]
+    m.detailDescription = profile["detailDescription"]
+    m.description = profile["description"]
+    m.signature = profile["signature"]
+    m.nickname = profile["nickname"]
+    m.gender = profile["gender"]
+    m.result = resp.text
+
+    print(m.userId)
+    return (m.userId)
+
+# 需要登录.
+def delete_list(listId):
+    url = "http://music.163.com/api/playlist/delete"
+    params = {"id": listId, "pid": listId}
+    
+    resp = requests.post(url, params, cookies=cookies, headers=header)
+    if resp.status_code == 200:
+        print("歌单删除成功...")
+
+
+# 需要登录.
+def create_list(name, userId):
+    url = "http://music.163.com/api/playlist/create"
+    params = {"name": name, "uid": userId}
+    print(urlencode(params))
+    resp = requests.post(url, urlencode(params), cookies=cookies, headers=header)
+    result = resp.json()
+    if result["code"] == 200:
+        print("创建成功..")
+        return result["id"]
+    else:
+        print(resp.text)
+        print("啥也没干")
+
+
+if __name__ == '__main__':
+    #uid = get_user_id_by_name("zhang")
+    #create_list("3333", uid)
+    delete_list(89898025)
---
+++ musician.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class musician:
+    userId = None
+    avatarUrl = None
+    nickname = None
+    gender = None
+    detailDescription = None
+    description = None
+    result = None
+    signature = None
+
+    def __init__(self):
+        pass
